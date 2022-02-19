9607b89814de7aceffa9ca7c12097dd2676dad7d
Lopez <steeven10f@gmail.com>
2022-02-19 16:49:09 -0500

    14

--- PrincipalApp/urls.py
+++ PrincipalApp/urls.py
@@ -3,11 +3,6 @@ from PrincipalApp import views
 from django.contrib.auth.decorators import login_required
 
 app_name = 'Tienda'
-urlpatterns = [
-   path('Index/',  views.Index.as_view(), name='Index'),
-   path('Login/', views.Login.as_view(), name='Login'),
-   path('Registro/', views.Registro.as_view(), name='Registro'),
-   path('Logout/', views.LogoutUsuario.as_view(), name='Logout'),
-   path('RegistroProducto/',views.RegistroProducto.as_view(), name='RegistroProducto'),
-   path('Producto/<int:pk>/',  views.Producto.as_view(), name='Producto'),
+urlpatterns = [ 
+   
 ]
